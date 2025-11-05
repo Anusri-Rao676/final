fcaa805d5b5bcfb85204f4367fb2b674400f87a0
Anusri Rao <anusriraok@gmail.com>
2025-11-05 07:06:16 +0530

    Update test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from app import hello
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+      result = hello()
+        self.assertEqual(result,'hello')
+
+if __name__ == '__main__':
+    unittest.main()
